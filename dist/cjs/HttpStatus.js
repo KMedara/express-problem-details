cc93b8fd13aeb1ae1ac0527f418c3f01a7dc663a
Kevin Medara <kmedara2015@outlook.com>
2023-09-17 00:31:47 -0400

    sdfas

--- dist/cjs/HttpStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpStatus = void 0;
-/* eslint-disable no-unused-vars */
-var HttpStatus;
-(function (HttpStatus) {
-    HttpStatus[HttpStatus["BadRequest"] = 400] = "BadRequest";
-    HttpStatus[HttpStatus["InternalServerError"] = 500] = "InternalServerError";
-})(HttpStatus = exports.HttpStatus || (exports.HttpStatus = {}));
